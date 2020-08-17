95dd2d6b2b30767c7220269fc05198b02efaa73b
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-08-17 18:57:47 +0545

    tabcomponent

---
+++ src/components/TabComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+class TabComponent extends Component {
+  render() {
+    return (
+      <div>
+        <Tabs>
+          <TabList>
+            <Tab>Tab Link 1</Tab>
+            <Tab>Tab Link 1</Tab>
+            <Tab>Tab Link 1</Tab>
+          </TabList>
+        </Tabs>
+      </div>
+    );
+  }
+}
+export default TabComponent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
-import Header from '../components/Header';
-
 import React, { Component } from 'react';
+
+import Header from '../components/Header';
+import TabComponent from '../components/TabComponent';
 
 export default class Main extends Component {
   render() {
     return (
       <div>
         <Header />
+        <TabComponent />
       </div>
     );
   }
